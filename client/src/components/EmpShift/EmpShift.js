fb12f2fcbd74b4a0a831c2dc657f48ce3a6bdc45
Zheli Zhu <brianzzl@hotmail.com>
2019-09-10 10:23:36 -0400

    added employee view

--- client/src/components/EmpShift/EmpShift.js
+++ client/src/components/EmpShift/EmpShift.js
@@ -1,3 +1,29 @@
-// import React from "react";
-// import "./style.css";
+import React, { Component } from "react";
+import EmpNav from "./EmpNav";
+import ShiftCard from "./EmpShiftCard";
+import shifts from "../../fakedata/shift.json";
+
+class EmpShift extends Component{
+  render(){
+    return(
+      <div>
+        <EmpNav/>
+        <div>
+          <h1>Employee Page</h1>
+          {shifts.map((shifts,i) => (<ShiftCard {...shifts} key={i}/>)
+            )}
+
+        </div>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default EmpShift;
+
+
+
 
