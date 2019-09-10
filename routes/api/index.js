9a564b4c49079713a6d9b3ebfe8f9fa54a524ef9
Yating(Esther) Su <yaringsu@foxmail.com>
2019-09-10 11:49:54 -0400

    Add users route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const shiftRoutes = require("./shifts");
+const userRoutes = require("./users");
 const authRoutes = require("./auth");
 
 // Book routes
 router.use("/shifts", shiftRoutes);
 
 router.use("/auth", authRoutes);
+
+router.use("/users", userRoutes);
 
 module.exports = router;
