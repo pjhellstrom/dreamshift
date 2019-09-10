20d5197ffa229f99624f48b8c289cf1a961e4c1b
Eric Jobin <ejobin@gmail.com>
2019-09-10 16:39:59 -0400

    Have form for manager to add a shift

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
+// import NavTabs from "./components/NavTabs";
 import LandingPage from "./components/pages/LandingPage";
 
 // import Date from "./components/Date/Date";
@@ -9,7 +9,7 @@ import LandingPage from "./components/pages/LandingPage";
 
 import MgrShift from "./components/MgrShift/MgrShift";
 import MgrTeam from "./components/MgrShift/MgrTeam";
-
+import MgrAddShift from "./components/MgrShift/MgrAddShift";
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/managerview" component={MgrShift} />
         <Route exact path="/managerview/team" component={MgrTeam} />
-        <Route exact path="/managerview/addshift" component={MgrShift} />
+        <Route exact path="/managerview/addshift" component={MgrAddShift} />
         <Route exact path="/managerview/manageshift" component={MgrShift} />
 
         {/* <Route exact path="/employeeview" component={EmpShift} /> */}
