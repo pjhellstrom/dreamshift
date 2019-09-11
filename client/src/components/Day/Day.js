45bc869c761e10aa1407faa4c0451239671213a6
Zheli Zhu <brianzzl@hotmail.com>
2019-09-11 10:53:20 -0400

    push before pull

--- client/src/components/Day/Day.js
+++ client/src/components/Day/Day.js
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
 import Date from "../Date";
 import EmpShiftCard from "../EmpShift";
-import API from "../../utils";
+// import API from "../../utils/api";
+import fakeData from "../../fakedata/shift.json"
 
 class Day extends Component{
   // state = {
-  //   time
+  //   pushedDay=[],
   // }
-  pushJob = () =>{
-    let jobData = API.
-  }
+
+
+
+  // pushJob = (day) =>{
+  //   // let jobData = API.getManager;
+  //   let jobData = fakeData;
+
+  // }
 
 
 }
