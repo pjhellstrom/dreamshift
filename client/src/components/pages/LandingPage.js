1f1605359c6cd7ddadb59a10ab6b6e89a5bda44a
pjhellstrom <pjh.hellstrom@gmail.com>
2019-09-17 10:37:40 -0400

    Added API import to LandingPage.js

--- client/src/components/pages/LandingPage.js
+++ client/src/components/pages/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import api from "../utils/api";
 import Button from "react-bootstrap/Button";
+import API from "../../utils/api";
 
 class LandingPage extends Component {
   state = {
