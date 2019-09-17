b31f2bcb4f64dc571446fc30d1aaa31b88edaccd
Eric Jobin <ejobin@gmail.com>
2019-09-16 21:57:04 -0400

    Added logging of user id to approve/reject shift functions

--- client/src/components/MgrShift/MgrApprvoalCard.js
+++ client/src/components/MgrShift/MgrApprvoalCard.js
@@ -6,29 +6,27 @@ import API from "../../utils/api";
 
 class MgrApprovalCard extends Component{
 
-	approve = (id) =>{
-		console.log(id)
+	approve = (shiftId, userId) =>{
+		console.log("shiftId " + shiftId);
+		console.log("userId " + userId);
 		// Make Call here
 	}
 
-	reject = (id) =>{
-		console.log(id)
+	reject = (shiftId, userId) =>{
+		console.log("shiftId " + shiftId);
+		console.log("userId " + userId);
 		// Make Call here
 	}
 
 	render(){
-		console.log("Approval Card")
 
 		if (this.props.pendingShifts.length != 0) {
-			
 			return (
 				<div className="card">
-
 					<Card>
 						<Card.Header>{this.props.firstName} {this.props.lastName}</Card.Header>
 						<Card.Body>
 							
-
 							{this.props.pendingShifts.map(
 						
 								(shift, i) => (
@@ -38,13 +36,20 @@ class MgrApprovalCard extends Component{
 										<b>Time:</b> {shift.start} to {shift.end}<br/>
 										<b>Have/Require:</b> {shift.claimed}/{shift.capacity}<br/>
 
-										<Button variant="outline-primary" onClick={() => this.approve(shift._id)}><i className="far fa-thumbs-up fa-2x"></i></Button>
-										<Button variant="outline-danger" onClick={() => this.reject(shift._id)}><i className="far fa-thumbs-down fa-2x"></i></Button>
+										<Button 
+											variant="outline-primary" 
+											onClick={() => this.approve(shift._id, this.props._id)}>
+												<i className="far fa-thumbs-up fa-2x"></i>
+										</Button>
+										<Button 
+											variant="outline-danger" 
+											onClick={() => this.reject(shift._id, this.props._id)}>
+											<i className="far fa-thumbs-down fa-2x"></i>
+										</Button>
 										<hr/>
 									</Card.Text>
 								)
 							)}
-
 
 						</Card.Body>
 						<Card.Footer >
